3b6f7b092fc88ac7e8638e28bd34e651bdf62ee5
Fernando J.A.T.C <it20265038@my.sliit.lk>
2022-09-07 13:13:07 +0530

    updated

--- resources/views/libraries/styles.blade.php
+++ resources/views/libraries/styles.blade.php
@@ -4,7 +4,7 @@
 <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
 <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-<link id="pagestyle" href="{{  asset('assets/css/argon-dashboard.min.css?v=2.0.5') }}" rel="stylesheet" />
+<link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.min.css') }}" rel="stylesheet" />
 
 <style>
     .async-hide {
