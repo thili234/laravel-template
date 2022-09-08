58c84a4a07c4a58c0c26615509d41d600688588d
Fernando J.A.T.C <it20265038@my.sliit.lk>
2022-09-08 10:04:41 +0530

    updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,54 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<title>
-    Argon Dashboard 2 PRO by Creative Tim
-  </title>
-
-
-<link rel="canonical" href="https://www.creative-tim.com/product/argon-dashboard-pro" />
-
-<meta name="keywords" content="creative tim, html dashboard, html css dashboard, web dashboard, bootstrap 5 dashboard, bootstrap 5, css3 dashboard, bootstrap 5 admin, Argon Dashboard bootstrap 5 dashboard, frontend, responsive bootstrap 5 dashboard, soft design, soft dashboard bootstrap 5 dashboard">
-<meta name="description" content="Argon Dashboard PRO is a beautiful Bootstrap 5 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you.">
-
-<meta name="twitter:card" content="product">
-<meta name="twitter:site" content="@creativetim">
-<meta name="twitter:title" content="Argon Dashboard PRO by Creative Tim">
-<meta name="twitter:description" content="Argon Dashboard PRO is a beautiful Bootstrap 5 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you.">
-<meta name="twitter:creator" content="@creativetim">
-<meta name="twitter:image" content="https://s3.amazonaws.com/creativetim_bucket/products/137/original/argon-dashboard-pro.jpg">
-
-<meta property="fb:app_id" content="655968634437471">
-<meta property="og:title" content="Argon Dashboard PRO by Creative Tim" />
-<meta property="og:type" content="article" />
-<meta property="og:url" content="https://demos.creative-tim.com/argon-dashboard-pro/pages/dashboards/landing" />
-<meta property="og:image" content="https://s3.amazonaws.com/creativetim_bucket/products/137/original/argon-dashboard-pro.jpg" />
-<meta property="og:description" content="Argon Dashboard PRO is a beautiful Bootstrap 5 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you." />
-<meta property="og:site_name" content="Creative Tim" />
+    <title>
+        Argon Dashboard 2 PRO by Creative Tim
+    </title>
 
 
+    <link rel="canonical" href="https://www.creative-tim.com/product/argon-dashboard-pro" />
 
+    <meta name="keywords"
+        content="creative tim, html dashboard, html css dashboard, web dashboard, bootstrap 5 dashboard, bootstrap 5, css3 dashboard, bootstrap 5 admin, Argon Dashboard bootstrap 5 dashboard, frontend, responsive bootstrap 5 dashboard, soft design, soft dashboard bootstrap 5 dashboard">
+    <meta name="description"
+        content="Argon Dashboard PRO is a beautiful Bootstrap 5 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you.">
 
+    <meta name="twitter:card" content="product">
+    <meta name="twitter:site" content="@creativetim">
+    <meta name="twitter:title" content="Argon Dashboard PRO by Creative Tim">
+    <meta name="twitter:description"
+        content="Argon Dashboard PRO is a beautiful Bootstrap 5 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you.">
+    <meta name="twitter:creator" content="@creativetim">
+    <meta name="twitter:image"
+        content="https://s3.amazonaws.com/creativetim_bucket/products/137/original/argon-dashboard-pro.jpg">
 
+    <meta property="fb:app_id" content="655968634437471">
+    <meta property="og:title" content="Argon Dashboard PRO by Creative Tim" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="https://demos.creative-tim.com/argon-dashboard-pro/pages/dashboards/landing" />
+    <meta property="og:image"
+        content="https://s3.amazonaws.com/creativetim_bucket/products/137/original/argon-dashboard-pro.jpg" />
+    <meta property="og:description"
+        content="Argon Dashboard PRO is a beautiful Bootstrap 5 admin dashboard with a large number of components, designed to look beautiful, clean and organized. If you are looking for a tool to manage dates about your business, this dashboard is the thing for you." />
+    <meta property="og:site_name" content="Creative Tim" />
 
-@include('libraries.styles')
-
-
-
-
-
+    @include('libraries.styles')
 
 </head>
+
 <body class="g-sidenav-show   bg-gray-100">
+    <div class="min-height-300 bg-primary position-absolute w-100"></div>
+     @include('components.side')
 
+     <main class="main-content position-relative border-radius-lg">
+        <!-- Navbar -->
+        @include('components.nav')
+        <!-- End Navbar -->
+        <div class="container-fluid py-4">
+            @yield('header')
 
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKDMSK6" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            @yield('content')
 
-@yield('content')
+            @include('components.footer')
+        </div>
+    </main>
 
-@include('libraries.scripts')
+    @include('libraries.scripts')
 </body>
+
 </html>
